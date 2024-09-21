0ad7d26ead30c708097f8623174530e3d5ab7df2
TotoBlunt <166468110+TotoBlunt@users.noreply.github.com>
2024-09-20 21:15:24 -0500

    corrigiendo

--- cod_IDL2.py
+++ cod_IDL2.py
@@ -62,17 +62,7 @@ if upload_file is not None:
         X_train, X_test, y_train, y_test = train_test_split(X[selected_features], y, test_size=0.2, random_state=42)
         model = RandomForestClassifier()
         model.fit(X_train, y_train)
-        '''
-        # Evaluar el modelo
-        st.write('### Metricas de Evaluacion del primer modelo: \n')
-        y_pred = model.predict(X_test)
-        st.write("#### Accuracy:", accuracy_score(y_test, y_pred))
 
-        # Convertir el classification report a un DataFrame
-        df_metrics = pd.DataFrame.from_records(
-            classification_report(y_test, y_pred, output_dict=True)
-        ).transpose()
-        st.table(df_metrics)'''
 
         #st.write(classification_report(y_test, y_pred))
 
